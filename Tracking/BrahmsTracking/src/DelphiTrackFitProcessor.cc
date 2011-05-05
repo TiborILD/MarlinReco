6108ddf142324382570d1758c10190ac110d8354
Steven Aplin <steven.aplin@desy.de>
2011-05-05 16:34:52 +0000

    added comments

--- Tracking/BrahmsTracking/src/DelphiTrackFitProcessor.cc
+++ Tracking/BrahmsTracking/src/DelphiTrackFitProcessor.cc
@@ -100,7 +100,7 @@ void DelphiTrackFitProcessor::processEvent( LCEvent * evt ) {
     
   //-- note: this will not be printed if compiled w/o MARLINDEBUG=1 !
 
-  streamlog_out(DEBUG) << "   processing event: " << _n_evt 
+  streamlog_out(DEBUG4) << "   processing event: " << _n_evt 
 		       << std::endl ;
    
   // get input collection and relations 
@@ -124,32 +124,30 @@ void DelphiTrackFitProcessor::processEvent( LCEvent * evt ) {
       
 	Track* track = dynamic_cast<Track*>( input_track_col->getElementAt( i ) ) ;
 		
-	IMarlinTrack* marlin_trk = new MarlinDelphiTrack(track) ;
+	IMarlinTrack* marlin_trk = new MarlinDelphiTrack(track) ; // here you can supply any track which has hits attached
 	
 	bool fit_success = marlin_trk->fit( false ) ; // SJA:FIXME: false means from out to in here i.e. backwards. This would be better if had a more meaningful name perhaps fit_fwd and fit_rev
 
-	if( fit_success ){ 
+	if( fit_success ){ // if the fit converged 
 
 	  TrackImpl* refittedTrack = marlin_trk->getIPFit() ;
 
 	  float point[3] ;
-	  //	  point[0] = point[1] = point[2] = 0.0 ; // IP
-	  point[0] = -1806. ;
-	  point[1] = -13.0 ;
-	  point[2] = 63.0 ; 
+	  point[0] = point[1] = point[2] = 0.0 ; // IP
 
-	  float r_cylinder = 500.0 ;
+	  float r_cylinder = 330.0 ;
 
+	  // SJA:FIXME: these need to be const so that no body can change them ... 
 	  //	  TrackImpl* nearestFit = marlin_trk->getNearestFitToPoint(point) ;	
 	  TrackImpl* nearestFit = marlin_trk->getNearestFitToCylinder(r_cylinder) ;	
 
 	  if(nearestFit){
-	    
+	   
 	    //  get ref and cov matrix for printing 
 	    const float* ref = nearestFit->getReferencePoint() ;
 	    EVENT::FloatVec cov = nearestFit->getCovMatrix() ;
 	    //	    streamlog_out( DEBUG ) << " MarlinDelphi track parameters: nearest to " << point[0] << " " << point[1] <<  " " << point[2] << " : "
-	    streamlog_out( DEBUG ) << " MarlinDelphi track parameters: nearest to cylinder " << r_cylinder << " : "
+	    streamlog_out( DEBUG4 ) << " MarlinDelphi track parameters: nearest to cylinder r = " << r_cylinder << " : "
 				   << " chi2/ndf " <<  nearestFit->getChi2() /  nearestFit->getNdf()  
 				   << " chi2 " <<  nearestFit->getChi2() << std::endl 
 	      
@@ -161,7 +159,16 @@ void DelphiTrackFitProcessor::processEvent( LCEvent * evt ) {
 				   << "\t ref : [" << ref[0] << ", " << ref[1] << ", "  << ref[2] << "]"     
 				   << std::endl ;
 
-	    // create NEW track to be returned. The responsiblitiy for deletion lies with the caller.
+
+	    // now we have the parameters of the track at the surface which is set to be the same as the hit ... though this won't work for the vertex as they are planes ...
+	    // if we want to check the distance to a hit and use the errors from the covariance matrix we will need to convert the d0 parameter into x and y with errors ... I'll do this tomorrow ...
+	    // to do things properly what we really need is a propagate to cylinder or plane method in addition to the propagate to point,
+	    // even without material this would a lot more preferable than fixing the surfaces in advance, we just need somebody to do it ;)
+
+	    // the fixing the surface in advance will work but we will need to check carefully that the extrapolation surfaces are really at the same surface of the hits, which won't be to hard to ensure.
+	    // at the moment due the fact that by definition d0 and z0 are both zero you have the x y z info as this is the reference point, we just need the errors as mentioned above. 
+	    // then we would get the hit and make the comparison ... 
+
 	    IMPL::TrackImpl* copyTrk = new IMPL::TrackImpl ;
 	    
 	    copyTrk->setD0( nearestFit->getD0() ) ;  
@@ -192,8 +199,13 @@ void DelphiTrackFitProcessor::processEvent( LCEvent * evt ) {
 	      { 
 		copyTrk->addHit( *it ) ;
 	      }
+
+	    // here we could add more hits to the track and create another track using 
+	    // IMarlinTrack* marlin_trk = new MarlinDelphiTrack(copyTrk) ;
+	    // it would be nice to extend IMarlinTrack so that it had an add hit and fit method, but that will take a bit of time ... 
+	    // so for now we should just create a new track each time we add a hit ... 
 	    
-	    //	    bool addtrack = true;
+	    //	    bool addtrack = false;
 	    bool addtrack = true;
 	    if( addtrack ) {
 	      trackVec->addElement( copyTrk ) ; // add it to the event ... 
@@ -214,8 +226,14 @@ void DelphiTrackFitProcessor::processEvent( LCEvent * evt ) {
 	      refittedTrack->subdetectorHitNumbers()[detIndex] = track->getSubdetectorHitNumbers()[detIndex] ;
 	    }
 	  
-	  //	  trackVec->addElement( refittedTrack );
-	  delete refittedTrack;
+	  //	    bool addtrack = true;
+	  bool addtrack = false;
+	  if( addtrack ) {
+	    trackVec->addElement( refittedTrack ) ; // add it to the event ... 
+	  } else {
+	    delete refittedTrack ; // or delete it 
+	  } 
+	  
 	}
 	
 	delete marlin_trk;
